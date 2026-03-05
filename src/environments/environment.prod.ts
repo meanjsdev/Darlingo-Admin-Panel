c517adca651c46434045e48a7d3fa4c7ce451dbb
LENOVO <alok.mishra@ongraph.com>
2026-03-05 12:35:22 +0530

    updated base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://18.206.171.255:5000/api', // Update with your production API URL
+  apiUrl: 'https://api.darlingo.co/api', // Update with your production API URL
 };
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://18.206.171.255:5000/api', // Update with your production API URL
-  };
-  
+  production: true,
+  apiUrl: 'https://api.darlingo.co/api', // Update with your production API URL
+};
